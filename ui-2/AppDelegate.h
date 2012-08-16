05f18cd832bf9e573d3d95e1eabdfb4882a583ec
Ibokan <ibokan@5-28.local>
2012-08-16 10:08:52 +0800

    Initial Commit

---
+++ ui-2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ui-2
+//
+//  Created by Ibokan on 12-8-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
